c936599724306b5c349d84489f7796b935965964
aldangold <meloshnikoff@gmail.com>
2022-04-17 01:03:34 +0300

    step11: added rollbar/react

--- src/init.js
+++ src/init.js
@@ -4,6 +4,7 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 import App from './App.jsx';
 import store from './slices/index.js';
+import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
 import { AuthProvider } from './contexts/authContext.jsx';
 import SocketContext from './contexts/socketContext.jsx';
 import { actions as messagesActions } from './slices/messagesSlice.js';
@@ -17,6 +18,15 @@ import 'regenerator-runtime';
 if (process.env.NODE_ENV !== 'production') {
     localStorage.debug = 'chat:*';
   }
+
+const rollbarConfig = {
+  accessToken: process.env.ROLLBAR,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
 
 export default async (socket) => {
   await i18n
@@ -48,14 +58,18 @@ export default async (socket) => {
       });
 
     return (
-        <Provider store={store}>
-          <I18nextProvider i18n={i18n}>
-            <AuthProvider>
-              <SocketContext.Provider value={socket}>
-                <App />
-              </SocketContext.Provider>
-            </AuthProvider>
-          </I18nextProvider>
-        </Provider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <I18nextProvider i18n={i18n}>
+              <AuthProvider>
+                <SocketContext.Provider value={socket}>
+                  <App />
+                </SocketContext.Provider>
+              </AuthProvider>
+            </I18nextProvider>
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
     );
   };
